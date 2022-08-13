009f3496b78a9751aabec0fcd5964389ecf5a98b
VenireVidereVincere <alan_jurado_canseco@hotmail.com>
2022-08-13 14:10:28 -0500

    Updated pkg.json and refactored mongoose db connection

---
+++ src/app.js
@@ -0,0 +1,13 @@
+//requiring dependencies
+const express = require("express")
+require("./db/mongoose")
+const usersRouter = require("./routers/users")
+const tasksRouter = require("./routers/tasks")
+
+const app = express()
+
+app.use(express.json())
+app.use(usersRouter)
+app.use(tasksRouter)
+
+module.exports = app
--- src/index.js
+++ src/index.js
@@ -1,25 +1,9 @@
 //requiring dependencies
-const express = require("express")
-require("./db/mongoose")
-const usersRouter = require("./routers/users")
-const tasksRouter = require("./routers/tasks")
-
-
-const app = express()
+const app = require("./app")
 
 // process.env.PORT is the port used by Heroku, the host of the app.
 const port = process.env.PORT
 
-
-app.use(express.json())
-app.use(usersRouter)
-app.use(tasksRouter)
-
 app.listen(port,()=>{
     console.log("Server is up on port " + port)
 })
-
-const Task = require("./models/task")
-const User = require("./models/user")
-
-
